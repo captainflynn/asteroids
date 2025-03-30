2b889839151554111609a3799a6bd1f46ed1bb95
captainflynn <vincent@nuszbaum.nl>
2025-03-31 00:22:18 +0200

    splitting

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def main():
                 if asteroid.collision(shot):
                     shot.kill()
                     asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
         
